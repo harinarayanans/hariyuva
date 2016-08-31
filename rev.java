da372915575fadb60eab1e780c59576e99a9d74a
harinarayanans <hariyuva1995@gmail.com>
2016-08-31 21:54:52 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		while(n!=0)
+		{
+		    int temp=n%10;
+		    System.out.print(temp);
+		    n=n/10;
+		    
+		}
+	}
+}
