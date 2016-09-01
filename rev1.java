ad125217bdbb02db778360a4497d2a5896f85517
harinarayanans <hariyuva1995@gmail.com>
2016-09-01 23:00:01 +0530

    Rename rev.java to rev1.java

---
+++ rev1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class rev
+{
+	public static void main (String[] args) 
+	{
+		Scanner ina=new Scanner(System.in);
+		String a=ina.nextLine();
+		StringBuilder sb=new StringBuilder(a);
+		System.out.println(sb.reverse());
+				
+		
+	
+	}
+}
